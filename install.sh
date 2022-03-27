5f942683001c8a51cdca3e92f2ae1ce3c499d034
Ryan Swanson <ryan.s.swanson@gmail.com>
2022-03-26 22:56:30 -0400

    adding vscode settings and quick and dirty install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+REPO=~/git/dotfiles
+
+ln -s $REPO/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -s $REPO/.profile ~/.profile
+ln -s $REPO/.tmux.conf ~/.tmux.conf
+ln -s $REPO/.vimrc ~/.vimrc
+ln -s $REPO/.gitconfig ~/.gitconfig
+ln -s $REPO/.mkshrc ~/.mkshrc
+ln -s $REPO/.zlogin ~/.zlogin
+ln -s $REPO/.zprofile ~/.zprofile
+ln -s $REPO/.zshrc ~/.zshrc
