0063cc7452752df8a7de7bada468713f252b68af
Ryan Swanson <ryan.s.swanson@gmail.com>
2022-03-26 23:47:46 -0400

    updating install.sh to add brew bundle and installs for oh-my-zsh and homebrew

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 REPO=~/git/dotfiles
 
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 ln -s $REPO/settings.json ~/Library/Application\ Support/Code/User/settings.json
 ln -s $REPO/.profile ~/.profile
 ln -s $REPO/.tmux.conf ~/.tmux.conf
@@ -9,3 +13,5 @@ ln -s $REPO/.mkshrc ~/.mkshrc
 ln -s $REPO/.zlogin ~/.zlogin
 ln -s $REPO/.zprofile ~/.zprofile
 ln -s $REPO/.zshrc ~/.zshrc
+
+brew bundle --file $REPO/Brewfile
