063265d58836876bddcb5547aacc955e8ba2e735
Ryan Swanson <ryan.s.swanson@gmail.com>
2022-03-27 20:07:46 -0400

    minor tweak to install for  zsh theme

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 ln -s $REPO/settings.json ~/Library/Application\ Support/Code/User/settings.json
-ln -s $REPO/swan.zsh-theme ~/.oh-my-zsh/themes/swan.zsh-theme
 ln -s $REPO/.profile ~/.profile
 ln -s $REPO/.tmux.conf ~/.tmux.conf
 ln -s $REPO/.vimrc ~/.vimrc
@@ -14,5 +13,9 @@ ln -s $REPO/.mkshrc ~/.mkshrc
 ln -s $REPO/.zlogin ~/.zlogin
 ln -s $REPO/.zprofile ~/.zprofile
 ln -s $REPO/.zshrc ~/.zshrc
+
+# ohmyzsh didn't like a symlink so we gotta do a filecopy
+# thankfully the theme is pretty static
+cp $REPO/swan.zsh-theme ~/.oh-my-zsh/themes/swan.zsh-theme
 
 brew bundle --file $REPO/Brewfile
